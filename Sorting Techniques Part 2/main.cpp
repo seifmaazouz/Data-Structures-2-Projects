8fe343744acc2c414a7ecb1bd5d2f140125998b7
seifmaazouz <154283002+seifmaazouz@users.noreply.github.com>
2025-03-14 20:51:29 +0200

    Change merge vector to an array
    An array is faster in this case as we pre-allocates all the memory needed instead of using dynamic allocation in vector.

--- Sorting Techniques Part 2/main.cpp
+++ Sorting Techniques Part 2/main.cpp
@@ -96,35 +96,33 @@ void printArray(int arr[], int n) {
 
 void mergeSort(int arr[], int low, int high) {
     if (low >= high) return;
-    int mid = (low + high)/ 2;
+    int mid = low + (high - low) / 2;
     mergeSort(arr, low, mid);
     mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
 }
 
 void merge(int arr[], int low, int mid, int high) {
-    vector<int> temp;
+    int size = high - low + 1;
+    int temp[size];
+    int index = 0;
     int left = low;
     int right = mid + 1;
     while (left <= mid && right <= high) {
         if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
+            temp[index++] = arr[left++];
         }
         else {
-            temp.push_back(arr[right]);
-            right++;
+            temp[index++] = arr[right++];
         }
     }
     while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
+        temp[index++] = arr[left++];
     }
     while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
+        temp[index++] = arr[right++];
     }
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
+    for (int i = 0; i < size; i++) {
+        arr[low + i] = temp[i];
     }
 }
